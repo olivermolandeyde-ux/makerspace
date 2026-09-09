317eba4fb8fe77165ae91c96aad6b44420ecaedd
Oliver Moland Eyde <olivermoland@Oliver-sin-MacBook-Air.local>
2026-09-09 13:32:08 +0200

    navbar done - Oliver

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,15 +5,16 @@ export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false)
 
   return (
-    <nav className="bg-[#ede8e8f2] p-1 shadow-sm flex items-center gap-4">
+    <nav className="bg-[#ede8e8f2] p-1 shadow-sm flex items-center gap-4 relative">
       <img className="h-16 w-12" src={MakerSpaceLogo} alt="Makerspace logo" />
       <h1 className="font-serif font-bold text-3xl text-gray-800">Makerspace</h1>
 
       {/* pc menu */}
-      <div className="hidden md:flex">
-        <a href="#" className="p-2">hjem</a>
-        <a href="#" className="p-2">service</a>
-        <a href="#" className="p-2">osv...</a>
+      <div className="hidden md:flex ml-auto font-extrabold gap-2">
+        <a href="#" className="p-2 hover:text-yellow-600 duration-100">hjem</a>
+        <a href="#" className="p-2 hover:text-yellow-600 duration-100">Info</a>
+        <a href="#" className="p-2 hover:text-yellow-600 duration-100">Produkter</a>
+        <a href="#" className="p-2 flex justify-center bg-yellow-500 w-40 rounded-4xl hover:bg-yellow-600 duration-100">Søknadskjema</a>
       </div>
 
       {/* mobile menu button */}
@@ -26,10 +27,11 @@ export default function Navbar() {
 
       {/* mobilmeny */}
       {isOpen && (
-        <div className="md:hidden absolute top-full left-0 w-full bg-[#ede8e8f2] flex flex-col gap-2 pb-2">
-          <a href="#" className="p-2">hjem</a>
-          <a href="#" className="p-2">service</a>
-          <a href="#" className="p-2">osv...</a>
+        <div className="md:hidden absolute top-full left-0 w-full bg-[#ede8e8f2] flex flex-col gap-1 pb-3 pt-2 text-xl font-extrabold">
+          <a href="#" className="p-2 hover:text-yellow-600 duration-100">Hjem</a>
+          <a href="#" className="p-2 hover:text-yellow-600 duration-100">Service</a>
+          <a href="#" className="p-2 hover:text-yellow-600 duration-100">Osv...</a>
+          <a href="#" className="p-3 flex justify-center bg-yellow-500 w-60 mx-auto rounded-4xl hover:bg-yellow-600 duration-100">Søknadskjema</a>
         </div>
       )}
     </nav>
