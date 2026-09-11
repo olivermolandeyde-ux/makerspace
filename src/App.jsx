8534fb80e87a9a6f5c843d8087ba6f7192da238c
Oliver Moland Eyde <olivermoland@Oliver-sin-MacBook-Air.local>
2026-09-11 12:55:13 +0200

    small fixed

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 import Home from './pages/Home';
 import About from './pages/About';
+import Products from './pages/products';
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
+          <Route path="/products" element={<Products />} />
         </Routes>
       </div>
     </Router>
