c8ebba2915ecb1ee095950846c1c63f2f28b7dfb
Oliver Moland Eyde <olivermoland@Oliver-sin-MacBook-Air.local>
2026-09-11 13:24:09 +0200

    motion framer

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,7 +2,6 @@ import HeroImage from '../assets/hero.jpg'
 import { motion, useScroll, useTransform } from 'framer-motion'
 
 export default function Hero() {
-
   const { scrollY } = useScroll()
   const y = useTransform(scrollY, [0, 500], [0, -150])
 
@@ -10,14 +9,14 @@ export default function Hero() {
     <section className="relative h-screen bg-cover bg-center flex items-center justify-center" style={{ backgroundImage: `url(${HeroImage})` }}>
       <div className="absolute inset-0 bg-black/50"></div>
 
-      <div className="relative z-10 text-center text-white px-4"> 
+      <motion.div style={{ y }} className="relative z-10 text-center text-white px-4">
         <h1 className="text-5xl md:text-7xl font-bold mb-4">
           Makerspace
         </h1>
         <p className="text-2xl md:text-4xl">
           Her skapes det greier
         </p>
-      </div>
+      </motion.div>
     </section>
   )
 }
